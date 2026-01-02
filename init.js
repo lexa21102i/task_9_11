856352e0b3f385ce8ada0c82d72d6bb3f859b124
Lexa21102i <lexa21102i@gmail.com>
2026-01-02 18:49:49 +0700

    change init

--- init.js
+++ init.js
@@ -2,6 +2,10 @@
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
+    document.querySelector('.card-title').innerHTML = `<span id="surnameOutput">${initPerson.firstName} ${initPerson.surname}</span>`;
 };
 
